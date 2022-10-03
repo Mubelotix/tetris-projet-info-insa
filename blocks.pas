f3620ffe165c5862c5a278ea4b6d9c339a44ba6b
Simon Girard <simon.girard@insa-rouen.fr>
2022-10-03 16:05:00 +0200

    Add load_blocks

--- blocks.pas
+++ blocks.pas
@@ -6,6 +6,36 @@ type Block = record
     tiles: Array [0..3, 0..3] of Byte;
 end;
 
+type BlockList = Array [0..6] of Block;
+
+function load_blocks(): BlockList;
+
 implementation
+
+
+function load_blocks(): BlockList;
+var f: Text;
+    i, x, y: Integer;
+    lines: Array [0..3] of String;
+begin
+    // Open file
+    Assign(f, 'blocks.txt');
+    Reset(f);
+
+    // Read lines
+    for y := 0 to 3 do begin
+        readln(f, lines[y]);
+    end;
+    close(f);
+
+    // Parse lines
+    for i := 0 to 6 do begin
+        for x := 0 to 3 do begin
+            for y := 0 to 3 do begin
+                load_blocks[i].tiles[x][y] := Ord(lines[y][1 + i*8 + x*2]) - Ord('0');
+            end;
+        end;
+    end;
+end;
 
 end.
---
+++ tetris.pas
@@ -0,0 +1,18 @@
+program tetris;
+uses blocks;
+
+var block_list: BlockList;
+var i, x, y: Integer;
+
+begin
+    block_list := load_blocks();
+    for i := 0 to 6 do begin
+        for x := 0 to 3 do begin
+            for y := 0 to 3 do
+                write(block_list[i].tiles[x][y]);
+            writeln();
+        end;
+        writeln();
+        writeln();
+    end;
+end.

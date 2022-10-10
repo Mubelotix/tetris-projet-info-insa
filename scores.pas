0335c498daaa920fe1305dc9b84b8790b6290f4f
Simon Girard <simon.girard@insa-rouen.fr>
2022-10-10 16:24:36 +0200

    Implement load_scores

--- scores.pas
+++ scores.pas
@@ -7,7 +7,10 @@ type Score = record
     value: Integer;
 end;
 
-type ScoreList = Array [0..100] of Score;
+type ScoreList = record
+	tab : Array [0..100] of Score;
+	length : integer;
+end;
 
 // Charge les scores depuis le fichier scores.txt
 function load_scores(): ScoreList;
@@ -21,7 +24,23 @@ procedure save_scores(scores: ScoreList);
 implementation
 
 function load_scores(): ScoreList;
-begin end;
+var score_data : Text;
+	i , n : integer;
+	
+	
+	begin
+		assign(score_data,'scores.txt');
+		reset (score_data);
+		readln(score_data,n);
+		for i:=1 to n div 2 do
+			begin
+				readln(score_data,load_scores.tab[i-1].pseudo);
+				readln(score_data,load_scores.tab[i-1].value);
+				load_scores.length := i;
+				
+
+			end;
+	end;
 
 procedure insert_score(var scores: ScoreList; new_score: Score);
 begin end;

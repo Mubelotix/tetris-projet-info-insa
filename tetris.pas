c5bad269ad84b493020e962e51ced6197b754de0
Mubelotix <mubelotix@gmail.com>
2022-11-02 21:43:00 +0100

    Fix issue

--- tetris.pas
+++ tetris.pas
@@ -7,8 +7,8 @@ var i, x, y: Integer;
 begin
     block_list := load_blocks();
     for i := 0 to 6 do begin
-        for x := 0 to 3 do begin
-            for y := 0 to 3 do
+        for y := 0 to 3 do begin
+            for x := 0 to 3 do
                 write(block_list[i].tiles[x][y]);
             writeln();
         end;

2af7d35270eddbc00bbb1a60aa388ac8fa756c94
Mubelotix <mubelotix@gmail.com>
2022-12-08 15:13:44 +0100

    Identification

--- tetris.pas
+++ tetris.pas
@@ -9,124 +9,82 @@ var falling_block: Block;
 var Key: char;
 var Activity: Boolean; //Si TRUE, le jeu est en cours
 
-
-
-
-
 procedure updateFollowingBlocks(); //Quand un bloc est fixe, en spawn un autre et update la liste des prochains blocs
 var i: integer;
- begin
- 
-  falling_block := following_blocks[0];  //Le prochain bloc qui spawn est le premier sur la liste
-  for i:=0 to 5 do following_blocks[i]:= following_blocks[i+1];  // Translation a droite de tous les blocs
-  following_blocks[6] := NewFallingBlock();
-
- end;
-
-
-
-
-procedure mainGame();  //Boucle principale
-var i:integer;
-  begin
-
-  display(Clone(MainGrid, falling_block), following_blocks, score);  //Afficher la grille et le bloc tombant
-
-     if test_collision(MainGrid, falling_block, falling_block.x, falling_block.y+1) = True then //Si le bloc a de la place il tombe
-      begin
-        falling_block.y := falling_block.y + 1;
-    
-      end
-     else    //Sinon la Grille fixe le bloc tombant dans sa structure
-      begin
-       MainGrid := (Clone(MainGrid, falling_block));
-       updateFollowingBlocks();
-
-      end;
-
-  if KeyPressed = True then 
-  begin
-  Key := ReadKey;  //Touche Droite et Gauche
-
-   Case Key
-      Of
-       #0    : Begin
-                if KeyPressed = True then Key:= ReadKey;
-                Case Key
-                Of
-                  #75 : 
-                  begin
-                  if test_collision(MainGrid, falling_block, falling_block.x-1, falling_block.y) = True then
-                  falling_block.x := falling_block.x - 1;
-                  end;
-                 #77 : 
-                 begin
-                 if test_collision(MainGrid, falling_block, falling_block.x+1, falling_block.y) = True then
-                 falling_block.x := falling_block.x + 1;
-                 end;
-                 
-                 #72 :
-                 begin
-                 if test_collision(MainGrid, rotate_block(falling_block, True), falling_block.x, falling_block.y) = True then
-                 falling_block := rotate_block(falling_block, True);
-                 end;
-
-                 End;
-                End;
-              
-  Else 
-  End;         
-  End;    
-  
-  
-for i:=3 to 23 do    //Verifie si une ligne est pleine et la detruit si c'est le cas
 begin
- if FullLineVerification(i, MainGrid)=True then 
-  begin
-  MainGrid := EraseLine(i, MainGrid);
-  score := score + 100;
-  end;        
+    falling_block := following_blocks[0];  //Le prochain bloc qui spawn est le premier sur la liste
+    for i:=0 to 5 do following_blocks[i]:= following_blocks[i+1];  // Translation a droite de tous les blocs
+        following_blocks[6] := NewFallingBlock();
 end;
 
-if Defeat(MainGrid) = True then 
- begin
- Activity := False;
+procedure mainGame(); //Boucle principale
+var i:integer;
+begin
+    display(Clone(MainGrid, falling_block), following_blocks, score);  //Afficher la grille et le bloc tombant
 
- end;
+    if test_collision(MainGrid, falling_block, falling_block.x, falling_block.y+1) = True then //Si le bloc a de la place il tombe
+        falling_block.y := falling_block.y + 1
+    end else begin //Sinon la Grille fixe le bloc tombant dans sa structure
+        MainGrid := (Clone(MainGrid, falling_block));
+        updateFollowingBlocks();
+    end;
 
-Delay(40);
-clrscr;
+    if KeyPressed = True then begin
+        Key := ReadKey;  //Touche Droite et Gauche
 
+        Case Key of
+            #0: Begin if KeyPressed = True then
+                Key := ReadKey;
+                Case Key Of
+                    #75: begin if test_collision(MainGrid, falling_block, falling_block.x-1, falling_block.y) = True then
+                        falling_block.x := falling_block.x - 1;
+                    end;
+                    #77: begin if test_collision(MainGrid, falling_block, falling_block.x+1, falling_block.y) = True then
+                        falling_block.x := falling_block.x + 1;
+                    end;
+                    #72: begin if test_collision(MainGrid, rotate_block(falling_block, True), falling_block.x, falling_block.y) = True then
+                        falling_block := rotate_block(falling_block, True);
+                    end;
+                End;
+            End;
+            Else
+        End;         
+    End;    
+
+    // Vérifie si une ligne est pleine et la detruit si c'est le cas
+    for i:=3 to 23 do begin
+        if FullLineVerification(i, MainGrid)=True then begin
+            MainGrid := EraseLine(i, MainGrid);
+            score := score + 100;
+        end;
+    end;
+
+    if Defeat(MainGrid) then
+        Activity := False;
+
+    Delay(40);
+    ClrScr();
 end;
-
-
-
 
 procedure gameLoop();
 begin
-
-   for i:=0 to 6 do   //initialisation de la liste des 7 premiers blocs qui tomberont
-     begin
-      following_blocks[i] := NewFallingBlock();
-   end;  
+    for i:=0 to 6 do // Initialisation de la liste des 7 premiers blocs qui tomberont
+        following_blocks[i] := NewFallingBlock();
     
-    
-MainGrid := empty_grid();
-display(MainGrid, following_blocks, score);
-updateFollowingBlocks();
+    MainGrid := empty_grid();
+    display(MainGrid, following_blocks, score);
+    updateFollowingBlocks();
 
-score:= 0;
-Activity := True;
-while Activity = True do mainGame();
-p:=1;                                                 //Sert a savoir si on choisit de recommencer ou pas
-while p<2 do   p := selectYorN(Key, Score, p);
+    score:= 0;
+    Activity := True;
+    while Activity do mainGame();
+    p:=1;   //Sert a savoir si on choisit de recommencer ou pas
+    while p<2 do p := selectYorN(Key, Score, p);
 
-if p=3 then gameloop();
-//if p:= -3 then {Menu principal}
+    if p=3 then gameloop();
 
+    //if p:= -3 then {Menu principal}
 end;
-
-
 
 
 
@@ -136,19 +94,7 @@ end;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 begin
-
     block_list := load_blocks();
     for i := 0 to 6 do begin
         for y := 0 to 3 do begin
@@ -159,13 +105,6 @@ begin
         writeln();
         writeln();
     end;
-    
-    
 
-gameLoop();
-
-
-
-
-
+    gameLoop();
 end.

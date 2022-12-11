4ca9504f287a7863b0a7163ca5a22db591d2395b
Mubelotix <mubelotix@gmail.com>
2022-12-11 03:28:36 +0100

    Renames

--- tetris.pas
+++ tetris.pas
@@ -12,10 +12,10 @@ var lines: Integer;
 var in_game: Boolean; //Si TRUE, le jeu est en cours
 var block_list, next_blocks: BlockList;
 var i, x, y, p, g, BestScore, DeletedLines: Integer;
-var MainGrid: Grid;
+var main_grid: Grid;
 var falling_block: Block;
-var ListOfScores: ScoreList;
-var ActualScore : Score;
+var scores: ScoreList;
+var current_score: Score;
 var Key: char;
 begin
     textures := initTextures();
@@ -24,7 +24,7 @@ begin
     IF scr=NIL THEN HALT;
     IF TTF_INIT<0 THEN HALT;
 
-    MainGrid := EmptyGrid();
+    main_grid := EmptyGrid();
     iteration := 10;
     lines := 0;
     last_key_pressed_iteration := 0;
@@ -40,13 +40,13 @@ begin
         should_render := False;
 
         // Fait tomber le bloc qui tombe
-        if CheckCollision(MainGrid, falling_block, falling_block.x, falling_block.y+1) then begin //Si le bloc a de la place il tombe
+        if CheckCollision(main_grid, falling_block, falling_block.x, falling_block.y+1) then begin //Si le bloc a de la place il tombe
             if (iteration mod 10) = 0 then begin
                 falling_block.y := falling_block.y + 1;
                 should_render := True;
             end;
         end else begin //Sinon la Grille fixe le bloc tombant dans sa structure
-            MainGrid := (Merge(MainGrid, falling_block));
+            main_grid := (Merge(main_grid, falling_block));
             UpdateNextBlocks(falling_block, next_blocks);
             should_render := True;
         end;
@@ -58,25 +58,25 @@ begin
         if (event.type_ = SDL_KEYDOWN) and (last_key_pressed_iteration + 10 < iteration) then begin
             last_key_pressed_iteration := iteration;
             if event.key.keysym.sym = SDLK_RIGHT then begin
-                if CheckCollision(MainGrid, falling_block, falling_block.x+1, falling_block.y) = True then begin
+                if CheckCollision(main_grid, falling_block, falling_block.x+1, falling_block.y) = True then begin
                     falling_block.x := falling_block.x + 1;
                     should_render := True;
                 end;
             end;
             if event.key.keysym.sym = SDLK_LEFT then begin
-                if CheckCollision(MainGrid, falling_block, falling_block.x-1, falling_block.y) = True then begin
+                if CheckCollision(main_grid, falling_block, falling_block.x-1, falling_block.y) = True then begin
                     falling_block.x := falling_block.x - 1;
                     should_render := True;
                 end;
             end;
             if event.key.keysym.sym = SDLK_UP then begin
-                if CheckCollision(MainGrid, rotate_block(falling_block, True), falling_block.x, falling_block.y+1) = True then begin
+                if CheckCollision(main_grid, rotate_block(falling_block, True), falling_block.x, falling_block.y+1) = True then begin
                     falling_block := rotate_block(falling_block, True);
                     should_render := True;
                 end;
             end;
             if event.key.keysym.sym = SDLK_DOWN then begin
-                if CheckCollision(MainGrid, falling_block, falling_block.x, falling_block.y+1) = True then begin
+                if CheckCollision(main_grid, falling_block, falling_block.x, falling_block.y+1) = True then begin
                     falling_block.y := falling_block.y + 1;
                     should_render := True;
                 end;
@@ -90,39 +90,39 @@ begin
         // Vérifie si une ligne est pleine et la detruit si c'est le cas
         DeletedLines:=0;
         for i:=3 to 23 do begin
-            if CheckFullLine(26-i, MainGrid) then begin
-                BlinkLine(MainGrid, 26-i, scr, textures, next_blocks, lines, ActualScore.value, BestScore, falling_block);
-                MainGrid := EraseLine(26-i, MainGrid);
+            if CheckFullLine(26-i, main_grid) then begin
+                BlinkLine(main_grid, 26-i, scr, textures, next_blocks, lines, current_score.value, BestScore, falling_block);
+                main_grid := EraseLine(26-i, main_grid);
                 DeletedLines := DeletedLines + 1;
                 should_render := True;
             end;
         end;
-        ActualScore.value := ActualScore.value + DeletedLines*100;
+        current_score.value := current_score.value + DeletedLines*100;
         lines := lines + DeletedLines;
 
         // Affiche le jeu si besoin
         if should_render then begin 
             SDL_FillRect(scr, nil, 0);
-            Display(Merge(MainGrid, falling_block), scr, textures, next_blocks, lines, ActualScore.value, BestScore);
+            Display(Merge(main_grid, falling_block), scr, textures, next_blocks, lines, current_score.value, BestScore);
             SDL_Flip(scr);
         end;
 
         // Attendre la prochaine frame
         delay(16);
         
-        if CheckDefeat(MainGrid) then HALT; {A Continuer ici}
+        if CheckDefeat(main_grid) then HALT; {A Continuer ici}
     end;
 
     // Quitter SDL
 
     
     //Enregistrement du score
-    insert_score(ListOfScores, ActualScore);
-    save_scores(ListOfScores);
+    insert_score(scores, current_score);
+    save_scores(scores);
 
     //Sert a savoir si on choisit de recommencer ou pas
     p:=1;                                                 
-    while ((p=1) or (p =(-1)) ) do p := selectYorN(Key, ActualScore.value, p);
+    while ((p=1) or (p =(-1)) ) do p := selectYorN(Key, current_score.value, p);
    
     if p=3 then gameloop(3);
     if p = -3 then gameloop(1);
